40dfefeef2f6caf69e0b5e8192698cda0ca34fbe
Matt Hilton <matt@mjhilton.net>
2017-10-31 10:34:24 +1100

    Created Calculator.js to solve all my difficulties with maths

---
+++ src/calculator.js
@@ -0,0 +1,17 @@
+(function() {
+    function addNumbers(number1, number2) {
+        return number1 + number2;
+    }
+
+    function subtractNumbers(number1, number2) {
+        return number1 - number2;
+    }
+
+    function divideNumbers(numerator, divisor) {
+        return numerator / divisor;
+    }
+
+    function multiplyNumbers(number1, number2) {
+        return number1 ^ number2;
+    }
+})();
