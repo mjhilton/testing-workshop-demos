c675ffa932d48b89144cfe021809984363fd5b77
Matt Hilton <matt@mjhilton.net>
2017-10-31 11:26:41 +1100

    Changed to Chai Should syntax

--- test/calculator-tests.js
+++ test/calculator-tests.js
@@ -1,8 +1,11 @@
-var expect = require('Chai').expect;
+// Chai's 'should' function attaches behaviour to all objects so you can use the shuold syntax. You could also just do require('Chai').should();
+var chai = require('Chai');
+chai.should();
+
 var calculator = require('../src/calculator');
 
 describe('Using the calculator to add 1 to 2', function() {
     it('should return 3', function() {
-        expect(calculator.add(1, 2)).to.equal(3);
+        calculator.add(1, 2).should.equal(3);
     });
 });
