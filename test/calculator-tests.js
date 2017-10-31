c8f04d9cc2a747c716eb2a16efe3cc9e0c717427
Matt Hilton <matt@mjhilton.net>
2017-10-31 11:18:24 +1100

    Implemented first test for Calculator.Add

---
+++ test/calculator-tests.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+var calculator = require('../src/calculator');
+
+describe('Using the calculator to add 1 to 2', function() {
+    it('should return 3', function() {
+        assert.equal(3, calculator.add(1, 2));
+    });
+});
