b781ff368473da4c3bd788fe69f502e121089b91
Matt Hilton <matt@mjhilton.net>
2017-10-31 11:17:52 +1100

    Fixed calculator to use module export

--- src/calculator.js
+++ src/calculator.js
@@ -1,26 +1,24 @@
-(function() {
-    function addNumbers(number1, number2) {
-        return number1 + number2;
-    }
+function addNumbers(number1, number2) {
+    return number1 + number2;
+}
 
-    function subtractNumbers(number1, number2) {
-        return number1 - number2;
-    }
+function subtractNumbers(number1, number2) {
+    return number1 - number2;
+}
 
-    function divideNumbers(numerator, divisor) {
-        return numerator / divisor;
-    }
+function divideNumbers(numerator, divisor) {
+    return numerator / divisor;
+}
 
-    function multiplyNumbers(number1, number2) {
-        return number1 ** number2;
-    }
+function multiplyNumbers(number1, number2) {
+    return number1 ^ number2;
+}
 
-    var calculator = {
-        add: addNumbers,
-        subtract: subtractNumbers,
-        divide: divideNumbers,
-        multiply: multiplyNumbers
-    };
+var calculator = {
+    add: addNumbers,
+    subtract: subtractNumbers,
+    divide: divideNumbers,
+    multiply: multiplyNumbers
+};
 
-    return calculator;
-})();
+module.exports = calculator;
