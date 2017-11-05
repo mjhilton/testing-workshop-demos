0e2deccc11b64e55048af733faf1993ee8e8b22f
Matt Hilton <matt@mjhilton.net>
2017-11-06 10:28:57 +1100

    Added more salary cases and refactored test code

--- test/salary-service-tests.js
+++ test/salary-service-tests.js
@@ -13,61 +13,57 @@ describe("Salary service", function() {
 
     describe("when calculating a Junior Dev's salary", function() {
         it("should return half the salary from the API", function() {
-            // Arrange
-            var expectedSalary = 35000;
             var jobTitle = "Junior Developer";
-            var apiReturnResult = {
-                job: jobTitle,
-                salary: 70000
-            };
+            var apiReturnedSalary = 70000;
+            var expectedSalary = 35000;
             
-            httpClient.get.resolves(JSON.stringify(apiReturnResult));
-
-            // Act
-            return salaryService.getSalary(jobTitle).then(result => {
-                // Assert
-                result.salary.should.equal(expectedSalary);
-            });
+            return assertSalaryCalculation(jobTitle, apiReturnedSalary, expectedSalary);
         });
     });
 
     describe("when calculating a Graduate Dev's salary", function() {
-        it("should return the full salary from the API", function() {
-            // Arrange
+        it("should return half the salary from the API", function() {
+            var jobTitle = "Graduate Developer";
+            var apiReturnedSalary = 60000;
             var expectedSalary = 30000;
-            var jobTitle = "Grad Developer";
-            var apiReturnResult = {
-                job: jobTitle,
-                salary: 60000
-            };
-
-            httpClient.get.resolves(JSON.stringify(apiReturnResult));
-
-            // Act
-            return salaryService.getSalary(jobTitle).then(result => {
-                // Assert
-                result.salary.should.equal(expectedSalary);
-            });
+            
+            return assertSalaryCalculation(jobTitle, apiReturnedSalary, expectedSalary);
         });
     });
 
     describe("when calculating a Senior Dev's salary", function() {
         it("should return the full salary from the API", function() {
-            // Arrange
-            var expectedSalary = 70000;
             var jobTitle = "Senior Developer";
-            var apiReturnResult = {
-                job: jobTitle,
-                salary: 70000
-            };
-
-            httpClient.get.resolves(JSON.stringify(apiReturnResult));
-
-            // Act
-            return salaryService.getSalary(jobTitle).then(result => {
-                // Assert
-                result.salary.should.equal(expectedSalary);
-            });
+            var apiReturnedSalary = 70000;
+            var expectedSalary = 70000;
+            
+            return assertSalaryCalculation(jobTitle, apiReturnedSalary, expectedSalary);
         });
     });
+
+    describe("when calculating an Architect's salary", function() {
+        it("should return the full salary from the API", function() {
+            var jobTitle = "Architect";
+            var apiReturnedSalary = 1000000;
+            var expectedSalary = 1000000;
+            
+            return assertSalaryCalculation(jobTitle, apiReturnedSalary, expectedSalary);
+        });
+    });
+
+    function assertSalaryCalculation(jobTitle, stubbedApiSalary, expectedSalary) {
+        // Arrange
+        var apiReturnResult = {
+            job: jobTitle,
+            salary: stubbedApiSalary
+        };
+
+        httpClient.get.resolves(JSON.stringify(apiReturnResult));
+        
+        // Act
+        return salaryService.getSalary(jobTitle).then(result => {
+            // Assert
+            result.salary.should.equal(expectedSalary);
+        });
+    };
 });
