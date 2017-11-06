eac0dbadb09c9eaf07ef7b20c8c4433a05289bc4
Matt Hilton <matt@mjhilton.net>
2017-11-06 15:49:47 +1100

    Added sample integration tests

--- integration_test/salary-app-test.js
+++ integration_test/salary-app-test.js
@@ -1,1 +1,33 @@
-var app = require("../server");
+var app = require('../server'),
+    httpClient = require('../src/http-client'),
+    db = require('../src/data-service');
+
+require('chai').should();
+
+describe("When requesting a JuniorDeveloper salary", function() {
+    it("returns a result", function() {
+        return httpClient.get("http://localhost:4000/api/salaries/Junior%20Developer")
+            .then(result => {
+                result = JSON.parse(result);
+                result.job.should.equal("Junior Developer");
+            });
+    });
+
+    it("increments a database counter", function() {
+        var keys = db.getKeys();
+        keys.length.should.equal(1);
+    });
+
+    after(function() {
+        db.clear();
+    });
+});
+
+describe("When requesting puppies", function() {
+    it("returns a puppy", function() {
+        return httpClient.get("http://localhost:4000/api/puppies")
+        .then(result => {
+           result.should.exist; 
+        });
+    })
+});
